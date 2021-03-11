95d4f8b52c1bf6d276e7cfa69f4a6cffaa86d172
Janjira Eriksson <janjira.eriksson@gmail.com>
2021-03-11 15:36:22 +0100

    Change function to class components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,59 +5,53 @@ import Footer from './components/Footer';
 import ContactCard from './components/ContactCard';
 import jokesData from "./components/jokesData"
 import Joke from "./components/Joke"
-import "./App.css";
-
 import Product from "./components/Product"
 import productsList from "./components/productList"
 import todosData from "./componentsTodo/todosData"
 import TodoItem from './componentsTodo/TodoItem';
+import "./App.css";
 
-function App() {
-
-  const jokeComponents = jokesData.map(joke => {
+class App extends React.Component {
+  render() {
+    const jokeComponents = jokesData.map(joke => {
+      return (
+        <Joke
+          question={joke.question}
+          punchLine={joke.punchLine}
+        />
+      )
+    })
+    const productComponents = productsList.map(item => <Product key={item.id} product={item} />)
+    const todoItems = todosData.map(item => <TodoItem key={item.id} item={item} />)
     return (
-      <Joke
-        question={joke.question}
-        punchLine={joke.punchLine}
-      />
-    )
-  })
-
-  const productComponents = productsList.map(item => <Product key={item.id} product={item} />)
-
-  const todoItems = todosData.map(item => <TodoItem key={item.id} item={item} />)
-
-  return (
-
-    <div className="app">
-      <Header />
-      <MainContent />
-      <Footer />
-      {todoItems}
-      {jokeComponents}
-      {productComponents}
-
-      <ContactCard
-        contact={{
-          name: "Mr.Whiskerson",
-          imgUrl: "http://placekitten.com/300/200",
-          phone: "(46) 555-61482",
-          email: "kitty@hotmail.com"
-        }}
-      />
-
-      <ContactCard
-        contact={{
-          name: "Mr. Fluffykins",
-          imgUrl: "http://placekitten.com/300/300",
-          phone: "(46) 515-61482",
-          email: "kitty@gmail.com"
-        }}
-
-
-      />
-    </div>
-  );
+      <div className="app">
+        <Header />
+        <MainContent />
+        <Footer />
+        {todoItems}
+        {jokeComponents}
+        {productComponents}
+        <ContactCard
+          contact={{
+            name: "Mr.Whiskerson",
+            imgUrl: "http://placekitten.com/300/200",
+            phone: "(46) 555-61482",
+            email: "kitty@hotmail.com"
+          }}
+        />
+        <ContactCard
+          contact={{
+            name: "Mr. Fluffykins",
+            imgUrl: "http://placekitten.com/300/300",
+            phone: "(46) 515-61482",
+            email: "kitty@gmail.com"
+          }}
+        />
+      </div>
+    );
+  }
 }
+
+
 
 export default App;
